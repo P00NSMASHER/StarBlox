563af315c8cc87adc453123f7e3657c79c878d09
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-18 14:49:05 -0400

    Load Home hero polish runtime

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './catalogArtRuntime';
 import { App } from './App';
 import './styles.css';
 import './release.css';
+import './homeHero.css';
+import './homeHeroRuntime';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
