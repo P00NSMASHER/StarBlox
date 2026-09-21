a8d9b5439e2f9e88e25de6da20ef31498ebd95cd
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-21 13:25:41 -0400

    home: load final reference tuning last

--- src/main.jsx
+++ src/main.jsx
@@ -37,6 +37,7 @@ import './progressionWidgets.css';
 import './environmentArt.css';
 import './mobileAccessibility.css';
 import './motionGameFeel.css';
+import './homeReferenceFinal.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
