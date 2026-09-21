fe2f0c0fc07abf65af7c66c127302776a66856cb
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-21 12:33:27 -0400

    test: remove temporary jsdom setup hook

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: '0.0.0.0' },
-  test: {
-    setupFiles: ['./src/testSetup.js']
-  }
+  server: { host: '0.0.0.0' }
 });
