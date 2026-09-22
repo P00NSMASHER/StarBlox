4407a6ccb6ed0b189fa4ec2a1ecf66275ead6305
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-22 10:43:34 -0400

    test: run motion observer checks in jsdom

--- src/motionGameFeelRuntime.test.js
+++ src/motionGameFeelRuntime.test.js
@@ -1,3 +1,4 @@
+// @vitest-environment jsdom
 import { describe, expect, it } from 'vitest';
 import { isPositiveFeedback, motionDurationFor, shouldScheduleMotionScan } from './motionGameFeelRuntime';
 
