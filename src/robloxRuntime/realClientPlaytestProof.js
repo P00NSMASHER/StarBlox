0d7bef508313f96a59670e7beab05b9a3bc3553c
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-25 17:31:34 -0400

    Keep src/robloxRuntime/realClientPlaytestProof.js retry horizon within release job budget

--- src/robloxRuntime/realClientPlaytestProof.js
+++ src/robloxRuntime/realClientPlaytestProof.js
@@ -85,7 +85,7 @@ export async function runRealClientPlaytestProof({
   timeoutMs=90_000,
   createRetryAttempts=12,
   createRetryBaseMs=2000,
-  createRetryMaxMs=90_000
+  createRetryMaxMs=60_000
 }){
   const task=await runOpenCloudLuauTask({
     apiKey,
